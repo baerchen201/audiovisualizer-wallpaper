aeebad079503989def3320eb72a16684a4139635
baer1 <videocreator@outlook.de>
2024-11-08 02:38:05 +0100

    Add basic JS structure

--- app.ts
+++ app.ts
@@ -1,3 +1,23 @@
 const DEV = !Object.keys(window).includes("wallpaperGetUtilities");
 
 console.log(DEV ? "Development environment" : "Wallpaper environment");
+
+function update(audio_data: number[]) {
+  console.log(audio_data);
+}
+
+if (DEV)
+  setInterval(
+    (function _() {
+      let rand_data = [];
+      while (rand_data.length < 128) {
+        rand_data.push(Math.random());
+      }
+
+      update(rand_data);
+
+      return _;
+    })(),
+    500
+  );
+else (window as any).wallpaperRegisterAudioListener(update);
